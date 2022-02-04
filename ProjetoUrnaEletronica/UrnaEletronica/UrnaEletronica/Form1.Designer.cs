d2fd83b1d518cba105374fcff33b3f8230977d06
Pablo Venino <pablo.venino@hotmail.com>
2022-02-04 11:14:06 -0300

    Add files via upload

---
+++ ProjetoUrnaEletronica/UrnaEletronica/UrnaEletronica/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿
+namespace UrnaEletronica
+{
+    partial class UrnaEletronica
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UrnaEletronica));
+            this.btn_num1 = new System.Windows.Forms.Button();
+            this.btn_num2 = new System.Windows.Forms.Button();
+            this.btn_num3 = new System.Windows.Forms.Button();
+            this.btn_num4 = new System.Windows.Forms.Button();
+            this.btn_num5 = new System.Windows.Forms.Button();
+            this.btn_num6 = new System.Windows.Forms.Button();
+            this.btn_num7 = new System.Windows.Forms.Button();
+            this.btn_num8 = new System.Windows.Forms.Button();
+            this.btn_num9 = new System.Windows.Forms.Button();
+            this.btn_num0 = new System.Windows.Forms.Button();
+            this.btn_branco = new System.Windows.Forms.Button();
+            this.btn_corrige = new System.Windows.Forms.Button();
+            this.btn_confirmar = new System.Windows.Forms.Button();
+            this.gpBox_vote = new System.Windows.Forms.GroupBox();
+            this.gpBox_containerDados = new System.Windows.Forms.GroupBox();
+            this.txtBox_PartidoCandidato = new System.Windows.Forms.TextBox();
+            this.txtBox_NomeCandidato = new System.Windows.Forms.TextBox();
+            this.picBox_FotoCandidato = new System.Windows.Forms.PictureBox();
+            this.txtBox_SecNum = new System.Windows.Forms.TextBox();
+            this.txtBox_PrimNum = new System.Windows.Forms.TextBox();
+            this.lb_LaranjaCorrige = new System.Windows.Forms.Label();
+            this.lb_VerdeConfirma = new System.Windows.Forms.Label();
+            this.lb_FraseAperte = new System.Windows.Forms.Label();
+            this.lb_Partido = new System.Windows.Forms.Label();
+            this.lb_NomePresidente = new System.Windows.Forms.Label();
+            this.lb_NumeroPresidente = new System.Windows.Forms.Label();
+            this.lb_Presidente = new System.Windows.Forms.Label();
+            this.lb_seuVotoPara = new System.Windows.Forms.Label();
+            this.gpBox_vote.SuspendLayout();
+            this.gpBox_containerDados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_FotoCandidato)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_num1
+            // 
+            resources.ApplyResources(this.btn_num1, "btn_num1");
+            this.btn_num1.BackColor = System.Drawing.Color.Black;
+            this.btn_num1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num1.FlatAppearance.BorderSize = 0;
+            this.btn_num1.ForeColor = System.Drawing.Color.White;
+            this.btn_num1.Name = "btn_num1";
+            this.btn_num1.UseVisualStyleBackColor = false;
+            this.btn_num1.Click += new System.EventHandler(this.btn_num1_Click);
+            // 
+            // btn_num2
+            // 
+            resources.ApplyResources(this.btn_num2, "btn_num2");
+            this.btn_num2.BackColor = System.Drawing.Color.Black;
+            this.btn_num2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num2.FlatAppearance.BorderSize = 0;
+            this.btn_num2.ForeColor = System.Drawing.Color.White;
+            this.btn_num2.Name = "btn_num2";
+            this.btn_num2.UseVisualStyleBackColor = false;
+            this.btn_num2.Click += new System.EventHandler(this.btn_num2_Click);
+            // 
+            // btn_num3
+            // 
+            resources.ApplyResources(this.btn_num3, "btn_num3");
+            this.btn_num3.BackColor = System.Drawing.Color.Black;
+            this.btn_num3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num3.FlatAppearance.BorderSize = 0;
+            this.btn_num3.ForeColor = System.Drawing.Color.White;
+            this.btn_num3.Name = "btn_num3";
+            this.btn_num3.UseVisualStyleBackColor = false;
+            this.btn_num3.Click += new System.EventHandler(this.btn_num3_Click);
+            // 
+            // btn_num4
+            // 
+            resources.ApplyResources(this.btn_num4, "btn_num4");
+            this.btn_num4.BackColor = System.Drawing.Color.Black;
+            this.btn_num4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num4.FlatAppearance.BorderSize = 0;
+            this.btn_num4.ForeColor = System.Drawing.Color.White;
+            this.btn_num4.Name = "btn_num4";
+            this.btn_num4.UseVisualStyleBackColor = false;
+            this.btn_num4.Click += new System.EventHandler(this.btn_num4_Click);
+            // 
+            // btn_num5
+            // 
+            resources.ApplyResources(this.btn_num5, "btn_num5");
+            this.btn_num5.BackColor = System.Drawing.Color.Black;
+            this.btn_num5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num5.FlatAppearance.BorderSize = 0;
+            this.btn_num5.ForeColor = System.Drawing.Color.White;
+            this.btn_num5.Name = "btn_num5";
+            this.btn_num5.UseVisualStyleBackColor = false;
+            this.btn_num5.Click += new System.EventHandler(this.btn_num5_Click);
+            // 
+            // btn_num6
+            // 
+            resources.ApplyResources(this.btn_num6, "btn_num6");
+            this.btn_num6.BackColor = System.Drawing.Color.Black;
+            this.btn_num6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num6.FlatAppearance.BorderSize = 0;
+            this.btn_num6.ForeColor = System.Drawing.Color.White;
+            this.btn_num6.Name = "btn_num6";
+            this.btn_num6.UseVisualStyleBackColor = false;
+            this.btn_num6.Click += new System.EventHandler(this.btn_num6_Click);
+            // 
+            // btn_num7
+            // 
+            resources.ApplyResources(this.btn_num7, "btn_num7");
+            this.btn_num7.BackColor = System.Drawing.Color.Black;
+            this.btn_num7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num7.FlatAppearance.BorderSize = 0;
+            this.btn_num7.ForeColor = System.Drawing.Color.White;
+            this.btn_num7.Name = "btn_num7";
+            this.btn_num7.UseVisualStyleBackColor = false;
+            this.btn_num7.Click += new System.EventHandler(this.btn_num7_Click);
+            // 
+            // btn_num8
+            // 
+            resources.ApplyResources(this.btn_num8, "btn_num8");
+            this.btn_num8.BackColor = System.Drawing.Color.Black;
+            this.btn_num8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num8.FlatAppearance.BorderSize = 0;
+            this.btn_num8.ForeColor = System.Drawing.Color.White;
+            this.btn_num8.Name = "btn_num8";
+            this.btn_num8.UseVisualStyleBackColor = false;
+            this.btn_num8.Click += new System.EventHandler(this.btn_num8_Click);
+            // 
+            // btn_num9
+            // 
+            resources.ApplyResources(this.btn_num9, "btn_num9");
+            this.btn_num9.BackColor = System.Drawing.Color.Black;
+            this.btn_num9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num9.FlatAppearance.BorderSize = 0;
+            this.btn_num9.ForeColor = System.Drawing.Color.White;
+            this.btn_num9.Name = "btn_num9";
+            this.btn_num9.UseVisualStyleBackColor = false;
+            this.btn_num9.Click += new System.EventHandler(this.btn_num9_Click);
+            // 
+            // btn_num0
+            // 
+            resources.ApplyResources(this.btn_num0, "btn_num0");
+            this.btn_num0.BackColor = System.Drawing.Color.Black;
+            this.btn_num0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num0.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_num0.FlatAppearance.BorderSize = 0;
+            this.btn_num0.ForeColor = System.Drawing.Color.White;
+            this.btn_num0.Name = "btn_num0";
+            this.btn_num0.UseVisualStyleBackColor = false;
+            this.btn_num0.Click += new System.EventHandler(this.btn_num0_Click);
+            // 
+            // btn_branco
+            // 
+            resources.ApplyResources(this.btn_branco, "btn_branco");
+            this.btn_branco.BackColor = System.Drawing.Color.White;
+            this.btn_branco.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_branco.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_branco.Name = "btn_branco";
+            this.btn_branco.UseVisualStyleBackColor = false;
+            // 
+            // btn_corrige
+            // 
+            resources.ApplyResources(this.btn_corrige, "btn_corrige");
+            this.btn_corrige.BackColor = System.Drawing.Color.OrangeRed;
+            this.btn_corrige.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_corrige.FlatAppearance.BorderColor = System.Drawing.Color.OrangeRed;
+            this.btn_corrige.Name = "btn_corrige";
+            this.btn_corrige.UseVisualStyleBackColor = false;
+            this.btn_corrige.Click += new System.EventHandler(this.btn_corrige_Click);
+            // 
+            // btn_confirmar
+            // 
+            resources.ApplyResources(this.btn_confirmar, "btn_confirmar");
+            this.btn_confirmar.BackColor = System.Drawing.Color.Green;
+            this.btn_confirmar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_confirmar.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.btn_confirmar.FlatAppearance.BorderSize = 0;
+            this.btn_confirmar.Name = "btn_confirmar";
+            this.btn_confirmar.UseVisualStyleBackColor = false;
+            // 
+            // gpBox_vote
+            // 
+            resources.ApplyResources(this.gpBox_vote, "gpBox_vote");
+            this.gpBox_vote.BackColor = System.Drawing.Color.Silver;
+            this.gpBox_vote.Controls.Add(this.gpBox_containerDados);
+            this.gpBox_vote.Controls.Add(this.btn_num0);
+            this.gpBox_vote.Controls.Add(this.btn_confirmar);
+            this.gpBox_vote.Controls.Add(this.btn_num7);
+            this.gpBox_vote.Controls.Add(this.btn_num8);
+            this.gpBox_vote.Controls.Add(this.btn_num9);
+            this.gpBox_vote.Controls.Add(this.btn_num1);
+            this.gpBox_vote.Controls.Add(this.btn_corrige);
+            this.gpBox_vote.Controls.Add(this.btn_num2);
+            this.gpBox_vote.Controls.Add(this.btn_num6);
+            this.gpBox_vote.Controls.Add(this.btn_branco);
+            this.gpBox_vote.Controls.Add(this.btn_num5);
+            this.gpBox_vote.Controls.Add(this.btn_num3);
+            this.gpBox_vote.Controls.Add(this.btn_num4);
+            this.gpBox_vote.Name = "gpBox_vote";
+            this.gpBox_vote.TabStop = false;
+            // 
+            // gpBox_containerDados
+            // 
+            resources.ApplyResources(this.gpBox_containerDados, "gpBox_containerDados");
+            this.gpBox_containerDados.Controls.Add(this.txtBox_PartidoCandidato);
+            this.gpBox_containerDados.Controls.Add(this.txtBox_NomeCandidato);
+            this.gpBox_containerDados.Controls.Add(this.picBox_FotoCandidato);
+            this.gpBox_containerDados.Controls.Add(this.txtBox_SecNum);
+            this.gpBox_containerDados.Controls.Add(this.txtBox_PrimNum);
+            this.gpBox_containerDados.Controls.Add(this.lb_LaranjaCorrige);
+            this.gpBox_containerDados.Controls.Add(this.lb_VerdeConfirma);
+            this.gpBox_containerDados.Controls.Add(this.lb_FraseAperte);
+            this.gpBox_containerDados.Controls.Add(this.lb_Partido);
+            this.gpBox_containerDados.Controls.Add(this.lb_NomePresidente);
+            this.gpBox_containerDados.Controls.Add(this.lb_NumeroPresidente);
+            this.gpBox_containerDados.Controls.Add(this.lb_Presidente);
+            this.gpBox_containerDados.Controls.Add(this.lb_seuVotoPara);
+            this.gpBox_containerDados.Name = "gpBox_containerDados";
+            this.gpBox_containerDados.TabStop = false;
+            // 
+            // txtBox_PartidoCandidato
+            // 
+            resources.ApplyResources(this.txtBox_PartidoCandidato, "txtBox_PartidoCandidato");
+            this.txtBox_PartidoCandidato.Name = "txtBox_PartidoCandidato";
+            // 
+            // txtBox_NomeCandidato
+            // 
+            resources.ApplyResources(this.txtBox_NomeCandidato, "txtBox_NomeCandidato");
+            this.txtBox_NomeCandidato.Name = "txtBox_NomeCandidato";
+            // 
+            // picBox_FotoCandidato
+            // 
+            resources.ApplyResources(this.picBox_FotoCandidato, "picBox_FotoCandidato");
+            this.picBox_FotoCandidato.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.picBox_FotoCandidato.Name = "picBox_FotoCandidato";
+            this.picBox_FotoCandidato.TabStop = false;
+            // 
+            // txtBox_SecNum
+            // 
+            resources.ApplyResources(this.txtBox_SecNum, "txtBox_SecNum");
+            this.txtBox_SecNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox_SecNum.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtBox_SecNum.Name = "txtBox_SecNum";
+            this.txtBox_SecNum.ReadOnly = true;
+            // 
+            // txtBox_PrimNum
+            // 
+            resources.ApplyResources(this.txtBox_PrimNum, "txtBox_PrimNum");
+            this.txtBox_PrimNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox_PrimNum.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtBox_PrimNum.Name = "txtBox_PrimNum";
+            this.txtBox_PrimNum.ReadOnly = true;
+            // 
+            // lb_LaranjaCorrige
+            // 
+            resources.ApplyResources(this.lb_LaranjaCorrige, "lb_LaranjaCorrige");
+            this.lb_LaranjaCorrige.Name = "lb_LaranjaCorrige";
+            // 
+            // lb_VerdeConfirma
+            // 
+            resources.ApplyResources(this.lb_VerdeConfirma, "lb_VerdeConfirma");
+            this.lb_VerdeConfirma.Name = "lb_VerdeConfirma";
+            // 
+            // lb_FraseAperte
+            // 
+            resources.ApplyResources(this.lb_FraseAperte, "lb_FraseAperte");
+            this.lb_FraseAperte.Name = "lb_FraseAperte";
+            // 
+            // lb_Partido
+            // 
+            resources.ApplyResources(this.lb_Partido, "lb_Partido");
+            this.lb_Partido.Name = "lb_Partido";
+            // 
+            // lb_NomePresidente
+            // 
+            resources.ApplyResources(this.lb_NomePresidente, "lb_NomePresidente");
+            this.lb_NomePresidente.Name = "lb_NomePresidente";
+            // 
+            // lb_NumeroPresidente
+            // 
+            resources.ApplyResources(this.lb_NumeroPresidente, "lb_NumeroPresidente");
+            this.lb_NumeroPresidente.Name = "lb_NumeroPresidente";
+            // 
+            // lb_Presidente
+            // 
+            resources.ApplyResources(this.lb_Presidente, "lb_Presidente");
+            this.lb_Presidente.Name = "lb_Presidente";
+            // 
+            // lb_seuVotoPara
+            // 
+            resources.ApplyResources(this.lb_seuVotoPara, "lb_seuVotoPara");
+            this.lb_seuVotoPara.Name = "lb_seuVotoPara";
+            // 
+            // UrnaEletronica
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.gpBox_vote);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "UrnaEletronica";
+            this.gpBox_vote.ResumeLayout(false);
+            this.gpBox_containerDados.ResumeLayout(false);
+            this.gpBox_containerDados.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_FotoCandidato)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_num1;
+        private System.Windows.Forms.Button btn_num2;
+        private System.Windows.Forms.Button btn_num3;
+        private System.Windows.Forms.Button btn_num4;
+        private System.Windows.Forms.Button btn_num5;
+        private System.Windows.Forms.Button btn_num6;
+        private System.Windows.Forms.Button btn_num7;
+        private System.Windows.Forms.Button btn_num8;
+        private System.Windows.Forms.Button btn_num9;
+        private System.Windows.Forms.Button btn_num0;
+        private System.Windows.Forms.Button btn_branco;
+        private System.Windows.Forms.Button btn_corrige;
+        private System.Windows.Forms.Button btn_confirmar;
+        private System.Windows.Forms.GroupBox gpBox_vote;
+        private System.Windows.Forms.GroupBox gpBox_containerDados;
+        private System.Windows.Forms.Label lb_FraseAperte;
+        private System.Windows.Forms.Label lb_Partido;
+        private System.Windows.Forms.Label lb_NomePresidente;
+        private System.Windows.Forms.Label lb_NumeroPresidente;
+        private System.Windows.Forms.Label lb_Presidente;
+        private System.Windows.Forms.Label lb_seuVotoPara;
+        private System.Windows.Forms.Label lb_LaranjaCorrige;
+        private System.Windows.Forms.Label lb_VerdeConfirma;
+        private System.Windows.Forms.TextBox txtBox_SecNum;
+        private System.Windows.Forms.TextBox txtBox_PrimNum;
+        private System.Windows.Forms.TextBox txtBox_PartidoCandidato;
+        private System.Windows.Forms.TextBox txtBox_NomeCandidato;
+        private System.Windows.Forms.PictureBox picBox_FotoCandidato;
+    }
+}
+
---
+++ ProjetoUrnaEletronica/UrnaEletronica/UrnaEletronica/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UrnaEletronica
+{
+    public partial class UrnaEletronica : Form
+    {
+        public UrnaEletronica()
+        {
+            InitializeComponent();
+        }
+        #region BotoesNumericos
+        private void btn_num1_Click(object sender, EventArgs e)
+        {
+            if(txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "1";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "1";
+                return;
+            }
+        }
+
+        private void btn_num2_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "2";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "2";
+                return;
+            }
+        }
+        private void btn_num3_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "3";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "3";
+                return;
+            }
+        }
+        private void btn_num4_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "4";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "4";
+                return;
+            }
+        }
+        private void btn_num5_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "5";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "5";
+                return;
+            }
+        }
+        private void btn_num6_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "6";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "6";
+                return;
+            }
+        }
+        private void btn_num7_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "7";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "7";
+                return;
+            }
+        }
+        private void btn_num8_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "8";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "8";
+                return;
+            }
+        }
+        private void btn_num9_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "9";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "9";
+                return;
+            }
+        }
+        private void btn_num0_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+            {
+                txtBox_PrimNum.Text = "0";
+                return;
+            }
+            if (txtBox_PrimNum.Text != "")
+            {
+                if (txtBox_SecNum.Text == "")
+                    txtBox_SecNum.Text = "0";
+                return;
+            }
+        }
+        #endregion
+
+
+        private void btn_corrige_Click(object sender, EventArgs e)
+        {
+            if (txtBox_PrimNum.Text == "")
+                MessageBox.Show("Não há nada a corrigir.");
+            if (txtBox_PrimNum.Text != "" && txtBox_SecNum.Text == "")
+                txtBox_PrimNum.Clear();
+            if (txtBox_SecNum.Text != "")
+                txtBox_SecNum.Clear();
+        }
+        private void NumerosCandidatos()
+        {
+            if(txtBox_PrimNum.Text != "" && txtBox_SecNum.Text == "")
+            {
+                CandidatoGru();
+            }
+        }
+
+        private void CandidatoGru()
+        {
+            if (txtBox_PrimNum.Text == "2" && txtBox_SecNum.Text == "8")
+            {
+                txtBox_NomeCandidato.Text = "Felonious Gru";
+                txtBox_PartidoCandidato.Text = "MPF - Meu Presidente Favorito";
+            }
+        }
+    }
+}
